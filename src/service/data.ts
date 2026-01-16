7f4a6d8e2af3825335080d88c43c17898a5ce09b
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-01-15 23:26:03 -0300

    feat: show data not formated

---
+++ src/service/data.ts
@@ -0,0 +1,14 @@
+export const data = `
+SUPERMERCADO IDEAL LTDA
+CNPJ: 23.456.789/0001-10
+
+CUPOM FISCAL
+15/01/2026  16:41
+
+DESC              QT  VL UNIT   VL TOTAL
+Leite Integral     2   4,79      9,58
+Pao Forma          1   7,90      7,90
+
+TOTAL R$ 17,48
+Pagamento: Débito
+`
