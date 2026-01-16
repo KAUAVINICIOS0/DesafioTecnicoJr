7c7563483b753664e008e0d07e029e6d356422c5
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-01-15 23:32:15 -0300

    feat: implements button on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { data } from "@/service/data";
 
 export default function Home() {
   return (
     <Card className="min-h-screen grid place-items justify-center">
-      <pre>{data}</pre>
+      <pre className="">{data}</pre>
+      <Button className="cursor-pointer">Processar</Button>
     </Card>
   );
 }
